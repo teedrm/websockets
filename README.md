9468ede3fab5f07b0ff171dfda2efa2f2cab9a72
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-09 22:38:39 -0700

    Initialize 1-start

---
+++ README.md
@@ -0,0 +1,15 @@
+# Multi Page Ract Apps
+[ ] What is a MPA in React?
+[ ] How do we achieve this
+[ ] Routing Libraries.  When to use, when its Overkill?
+
+# WebSockets
+[ ] What is WebSockets?
+[ ] WebSockets & http protocol
+[ ] WebSockets Libraries
+[ ] MultiPage Demo App
+[ ] Express Server
+[ ] Server & Server Connections
+[ ] Socket Events
+[ ] Custom Events
+[ ] Broadcast vs Direct messages
---
+++ react-client/src/App.js
@@ -0,0 +1,27 @@
+import Login from 'pages/Login';
+import Chat from 'pages/Chat';
+import {useState} from 'react';
+import 'App.css';
+
+export default function App() {
+  const [auth, setAuth] = useState(false);
+
+  // Perform some login process for the user
+  const login = function(email, password) {
+    setAuth(true);
+  };
+
+  const logout = function() {
+    setAuth(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Web Sockets React</h1>
+
+      {auth && <Chat logout={logout} />}
+      {!auth && <Login login={login} />}
+
+    </div >
+  );
+}
